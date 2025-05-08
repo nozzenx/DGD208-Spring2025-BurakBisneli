d4c46c42a0d98277c22b95fe21e14dbfc2ff4ac2
Burak Bisneli <bisneliburak@gmail.com>
2025-05-08 23:27:01 +0300

    create pet class
    * created pet class with hunger, sleep and fun int values.
    * added increase and decrease methods for each hunger, sleep and fun int values.

---
+++ DGD208-Spring2025-BurakBisneli/Pet.cs
@@ -0,0 +1,39 @@
+﻿namespace DGD208_Spring2025_BurakBisneli;
+
+public class Pet
+{
+    private int _hunger;
+    private int _sleep;
+    private int _fun;
+
+
+    public void IncreaseHunger(int amount)
+    {
+        _hunger += amount;
+    }
+
+    public void DecreaseHunger(int amount)
+    {
+        _hunger -= amount;
+    }
+
+    public void IncreaseSleep(int amount)
+    {
+        _sleep += amount;
+    }
+
+    public void DecreaseSleep(int amount)
+    {
+        _sleep -= amount;
+    }
+
+    public void IncreaseFun(int amount)
+    {
+        _fun += amount;
+    }
+
+    public void DecreaseFun(int amount)
+    {
+        _fun -= amount;
+    }
+}
