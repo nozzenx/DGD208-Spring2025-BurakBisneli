97c5ec55da2e7c0933a87b7cda59e400f9ffe037
Burak Bisneli <bisneliburak@gmail.com>
2025-06-01 22:18:24 +0300

    docs: added project plan
    * added ProjectPlan.txt

---
+++ DGD208-Spring2025-BurakBisneli/PetInventoryManager.cs
@@ -0,0 +1,17 @@
+﻿namespace DGD208_Spring2025_BurakBisneli;
+
+public class PetInventoryManager
+{
+    public List<Pet> CurrentPets = new List<Pet>();
+    
+    
+    public void AddPet(Pet pet)
+    {
+        CurrentPets.Add(pet);
+    }
+    
+    public void RemovePet(Pet pet)
+    {
+        CurrentPets.Remove(pet);
+    }
+}
