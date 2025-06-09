8af85699bfb64e66fa7a206f948c3e2f767bdaf8
Burak Bisneli <bisneliburak@gmail.com>
2025-06-09 21:21:42 +0300

    temp

--- DGD208-Spring2025-BurakBisneli/obj/Debug/net9.0/DGD208-Spring2025-BurakBisneli.AssemblyInfo.cs
+++ DGD208-Spring2025-BurakBisneli/obj/Debug/net9.0/DGD208-Spring2025-BurakBisneli.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DGD208-Spring2025-BurakBisneli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+367461cb62da54647eba562a7429c9752382fd78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6cd95822b991b0aa589a78d7199376f688b60d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DGD208-Spring2025-BurakBisneli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DGD208-Spring2025-BurakBisneli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
